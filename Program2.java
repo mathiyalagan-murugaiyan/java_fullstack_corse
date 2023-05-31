eeb71bc68653dccb1c7b1444e0dc2b619e7ef004
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-05-31 21:31:35 +0530

    Assignments

---
+++ Program2.java
@@ -0,0 +1,24 @@
+class Temprature{
+	
+	
+	
+	static float temp(int x){
+		
+	 
+		return (x -32)*(5.0f/9.0f);
+		 
+		
+	}
+	
+	public static void main(String args[]){
+		
+		
+	    
+		
+		System.out.println(temp(100));
+		
+		
+		
+	}
+	
+}
