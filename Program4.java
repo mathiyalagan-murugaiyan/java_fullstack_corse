a30eee6c463e69421d7ba87d985b3b1bf4147a92
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-06-05 20:36:48 +0530

    IF Condition

---
+++ Program4.java
@@ -0,0 +1,17 @@
+class Program4{
+
+	public static void main(String args[]){
+
+      int age = 23;
+
+      if (age >= 18)
+
+      	System.out.println("your eligible for vote...");
+
+      else
+
+      	System.out.println("your not eligible for vote...");
+
+
+	}
+}
