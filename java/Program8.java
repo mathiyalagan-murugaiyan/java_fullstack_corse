f034d9e2d945183f39576607d55b55d84164b646
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-06-07 22:01:32 +0530

    import Scanner class

--- java/Program8.java
+++ java/Program8.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Program8{
 
    static void factorial(int n){
@@ -16,7 +17,11 @@ System.out.println("factorial is :" + fact);
 
 
 	public static void main(String args[]){
+     
+     Scanner in = new Scanner(System.in);
+      
+      System.out.println("Enter the number");
 
-     factorial(5);
+     factorial(in.nextInt());
 	}
 }
