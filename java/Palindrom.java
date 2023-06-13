e35ed72489eeebe2041a7fe735b6145af09ef1f4
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-06-13 18:41:14 +0530

    Palindrom Program

---
+++ java/Palindrom.java
@@ -0,0 +1,28 @@
+class Palindrom{
+
+	public static void main(String args[]){
+
+		int r ,sum=0,temp;
+		int n= 432;
+
+		temp =n;
+
+		while (n>0){
+         
+         r=n%10;
+
+         sum = (sum*10) + r;
+
+         n = n/10;
+		}
+		 if (temp == sum){
+
+         	System.out.println("prime number");
+
+         }
+         else 
+
+         	System.out.println("not prime number");
+
+	}
+}
