91a9a4c465bf9f4b1217cde239c8f7d62f2aa516
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-06-14 22:23:00 +0530

    find the strong number

---
+++ java/StrongNumber.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class StrongNumber{
+
+    int num ;
+
+    
+
+   static String Strong(int num){
+   	
+    
+    int temp,fact,sum=0,rem,i;
+    String number ,value;
+    
+    temp = num;
+
+    while(num > 0){
+
+    	rem = num%10;
+
+    	fact=1;
+
+    	for(i=1;i<=rem;i++){
+          
+          fact = fact*i;
+          
+    	}
+
+    	sum = sum+fact;
+
+    	num = num/10;
+
+    }
+    return value=(temp == sum) ? "Strong number" : "not Strong number";
+
+      
+	}
+    
+   public static void main(String args[]){
+
+     Scanner in = new Scanner(System.in);
+     
+     System.out.println("Enter the value :");
+
+     String s = Strong(in.nextInt());
+
+     System.out.println(s);
+   }
+    
+}
